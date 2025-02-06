654a9b4d6d902bddc4cf6f1a54d31a4d8f0ce6b5
CCCU <CCCU@148.210.74.83>
2025-02-06 16:27:03 -0700

    Hice un cambio a x archivo

---
+++ src/main/java/com/mycompany/repositorio_prueba/Repositorio_prueba.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositorio_prueba;
+
+/**
+ *
+ * @author CCCU
+ */
+public class Repositorio_prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        int x;
+    }
+}
